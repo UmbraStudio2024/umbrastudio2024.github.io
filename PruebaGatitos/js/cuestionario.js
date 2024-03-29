338c048b62e72875d3e2e5a445abb89b94d394fe
Yerko Andrei <yerkoandrei@gmail.com>
2024-03-29 11:21:39 -0300

    base prueba gatitos

---
+++ PruebaGatitos/js/cuestionario.js
@@ -0,0 +1,190 @@
+// Inicialización
+var preguntaActual = 0;
+window.onload = function()
+{
+    var cuestionario = document.getElementById('cuestionario');
+    var resultados = document.getElementById('resultados');
+
+    cuestionario.style.display = "block";
+    resultados.style.display = "none";
+
+    actualizarContenido(preguntaActual);
+};
+
+function siguientePregunta(indice)
+{
+    preguntaActual ++;
+    if (preguntaActual < preguntas.length)
+    {
+        actualizarContenido(preguntaActual);
+    }
+    else
+    {
+        mostrarResultado();
+    }
+};
+
+function actualizarContenido(indice)
+{
+    var txtPregunta = document.getElementById('txtPregunta');
+    txtPregunta.innerHTML = '<h2 class="pregunta mt-1 ml-2">' + preguntas[indice].pregunta + '</h2>';
+
+    var imgPregunta = document.getElementById('imgPregunta');
+    imgPregunta.src = preguntas[indice].imagen;
+
+    var alternativasDivs = document.querySelectorAll('.alternativa');
+    for (var i = 0; i < alternativasDivs.length; i++)
+    {
+        alternativasDivs[i].innerHTML = '<button type="button" class="btn-sm btn-outline-primary text-center" onclick="siguientePregunta(' + indice + ')"><h4>' + preguntas[indice].alternativas[i] + '</h4></button>';
+    }
+};
+
+function mostrarResultado()
+{
+    var cuestionario = document.getElementById('cuestionario');
+    var resultados = document.getElementById('resultados');
+
+    cuestionario.style.display = "none";
+    resultados.style.display = "block";
+
+    // Cálculo variables
+
+
+
+    var txtResultado = document.getElementById('txtResultado');
+    txtResultado.innerHTML = '<h2 class="pregunta mt-1 ml-2">' + preguntas[indice].pregunta + '</h2>';
+
+    var imgResultado = document.getElementById('imgResultado');
+    imgResultado.src = preguntas[indice].imagen;
+};
+
+// 14 preguntar con sus variables
+var preguntas =
+[
+    {
+        pregunta: "1/14. Acabas de despertar, todos duermen en casa. Tu primer pensamiento es:",
+        imagen: "images/p1.png",
+        alternativas:
+        [
+            "Hoy viviré una nueva aventura.",                                                           // SENSACION
+            "Un día igual que ayer, igual que mañana"                                                   // INTUICION - PERCEPCION
+        ]
+    },
+    {
+        pregunta: "2/14. Tu Karen(cio) aún duerme",
+        imagen: "images/p1.png",
+        alternativas:       
+        [
+            "Le despertaré a lengüetazos ya hay que desayunar",                                         // SENTIMIENTO
+            "Mejor le dejaré dormir, puedo ir a tomar solcito por mientras"                             // PENSAMIENTO - JUICIO
+        ]
+    },
+    {
+        pregunta: "3/14. Estás en la cima de tu rascador y miras hacia el horizonte",
+        imagen: "images/p1.png",
+        alternativas:
+        [
+            "Solo piensas en tomar solcito y descansar un poco",                                        // SENTIMIENTO
+            "Te imaginas yendo a las casas hasta donde llega tu mirada"                                 // PENSAMIENTO
+        ]
+    },
+    {
+        pregunta: "4/14. Te encontraste con tu michi amigo apenas bajas del rascador",
+        imagen: "images/p1.png",
+        alternativas:
+        [
+            "Sueles comenzar la conversación",                                                          // EXTRAVERSION
+            "Esperas que el michi hable primero"                                                        // INTROVERSION
+        ]
+    },
+    {
+        pregunta: "5/14.  Karen(cio) se levantó y tiene una caja entre las manos, es un nuevo rascador de piso",
+        imagen: "images/p1.png",
+        alternativas:
+        [
+            "Lo observas detenidamente, ¿qué huele?,se ve algo extraño, ¿es amigo o enemigo?",          // PENSAMIENTO - PERCEPCION
+            "Te rascas las garritas como si no hubiera un mañana"                                       // SENTIMIENTO
+        ]
+    },
+    {
+        pregunta: "6/14. Las sorpresas no acaban, Karen(cio) te muestra dos juguetes nuevos, uno en cada mano",
+        imagen: "images/p1.png",
+        alternativas:
+        [
+            "Miras ambos y eliges el que más te gusta",                                                 // SENSACION - JUICIO
+            "No lo piensas, solo eliges el primero que viste"                                           // INTUICION
+        ]
+    },
+    {
+        pregunta: "7/14. Es tu momento de hacerte aseo",
+        imagen: "images/p1.png",
+        alternativas:
+        [
+            "Lo haces a la hora y en el lugar de siempre",                                              // INTUICION
+            "Cualquier lugar y cualquier momento es bueno para ello"                                    // SENSACION
+        ]
+    },
+    {
+        pregunta: "8/14. Tu michi amigo está sobre un árbol y te invita a subir",
+        imagen: "images/p1.png",
+        alternativas:
+        [
+            "Te motivas y vas con él sin dudarlo",                                                      // EXTRAVERSION
+            "Demasiado agotador, prefieres ir a tu cama a descansar"                                    // INTROVERSION - PERCEPCION
+        ]
+    },
+    {
+        pregunta: "9/14. En el camino de vuelta a casa te encuentras con una pelota de color amarillo",
+        imagen: "images/p1.png",
+        alternativas:
+        [
+            "Confías en tu experiencia, es inofensivo, puedo jugar con eso",                            // SENSACION - PERCEPCION
+            "Confías en tu intuición, podría venir su dueño a reclamarlo y salir lastimado"             // INTUICION - JUICIO
+        ]
+    },
+    {
+        pregunta: "10/14. Llegaron visitas a tu casa sin previo aviso",
+        imagen: "images/p1.png",
+        alternativas:
+        [
+            "Te acercas a la gente a compartir y juguetear",                                            // EXTRAVERSION
+            "Simplemente te vas, quieres soledad y tranquilidad"                                        // INTROVERSION
+        ]
+    },
+    {
+        pregunta: "11/14. Las visitas tratas de acercarte a ti para mimarte",
+        imagen: "images/p1.png",
+        alternativas:
+        [
+            "Permites que ellos se acerquen sin tener miedo",                                           // EXTRAVERSION
+            "Tienes puesto un pie en el acelerador, en cualquier momento sales corriendo"               // INTROVERSION/p/14
+        ]
+    },
+    {
+        pregunta: "12/14. Ya se fueron las visitas y es momento del paseo con tu Karen(cio)",
+        imagen: "images/p1.png",
+        alternativas:
+        [
+            "Prefieres los mismos lugares y hacer lo mismo",                                            // INTUICION
+            "Te gustaría que fueran a un lugar nuevo, nuevas emociones y sensaciones"                   // SENSACION 
+        ]
+    },
+    {
+        pregunta: "13/14. El paseo se acabó y es momento de volver",
+        imagen: "images/p1.png",
+        alternativas:
+        [
+            "Ya es momento de volver, es parte del plan",                                               // PENSAMIENTO - JUICIO
+            "Haces tiempo, el clima está agradable, no quieres volver aun"                              // SENTIMIENTO
+        ]
+    },
+    {
+        pregunta: "14/14. Es hora de dormir, tu Karen(cio) está en su cama, agotada(o) por el día de trabajo",
+        imagen: "images/p1.png",
+        alternativas:
+        [
+            "Le das su espacio, que ocupe toda la cama si quiere",                                     // PENSAMIENTO
+            "Necesita un poquito de amor, te acuestas en su regazo"                                    // SENTIMIENTO - PERCEPCION
+        ]
+    }
+];
